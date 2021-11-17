a804d46062e787d9efe3f744d5122fc2021819be
TsalachSnow <prosper.amalaha@decagon.dev>
2021-11-17 11:39:10 +0100

    second commit week5 task

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,46 +5,60 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Server{
+public class Server {
 
     public static void main(String[] args) throws IOException {
         ServiceImplementation si = new ServiceImplementation();
         ServerSocket server = new ServerSocket(8082);
-        while(server != null) {
+        ExecutorService foo = Executors.newFixedThreadPool(5);
+        while (server != null) {
             Socket clientSocket = server.accept();
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            StringBuilder requestBuilder = new StringBuilder();
-            String line;
-            while (!(line = br.readLine()).isBlank()) {
-                requestBuilder.append(line).append("\r\n");
-            }
+            foo.execute(() -> {
+                try {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            String request = requestBuilder.toString();
-            String[] requestsLines = request.split("\r\n");
-            String[] requestLine = requestsLines[0].split(" ");
-            String method = requestLine[0];
-            String path = requestLine[1];
-            String version = requestLine[2];
-            String host = requestsLines[1].split(" ")[1];
+                    StringBuilder requestBuilder = new StringBuilder();
+                    String line;
+                    while (!(line = br.readLine()).isBlank()) {
+                        requestBuilder.append(line).append("\r\n");
+                    }
+
+                    String request = requestBuilder.toString();
+                    String[] requestsLines = request.split("\r\n");
+                    String[] requestLine = requestsLines[0].split(" ");
+                    String method = requestLine[0];
+                    String path = requestLine[1];
+                    String version = requestLine[2];
+                    String host = requestsLines[1].split(" ")[1];
 
 
-            Path filePath = si.getFilePath("src/main/java/MyFiles/index.html");
-            Path filePath1 = si.getFilePath("src/main/java/MyFiles/index1.json");
-            Path filePath2 = si.getFilePath("src/main/java/MyFiles/error.html");
-            if (path.equals("/") && Files.exists(filePath)) {
-                String contentType = si.guessContentType(filePath);
-                si.sendResponse(clientSocket, "200 OK", contentType, Files.readAllBytes(filePath));
-            } else if (path.equals("/json") && Files.exists(filePath1)) {
-                String contentType = si.guessContentType(filePath1);
-                si.sendResponse(clientSocket, "200 OK", contentType, Files.readAllBytes(filePath1));
-            } else {
-                String contentType = si.guessContentType(filePath2);
-                si.sendResponse(clientSocket, "200 OK", contentType, Files.readAllBytes(filePath2));
-            }
-            System.out.println(requestBuilder.toString());
+                    Path filePath = si.getFilePath("src/main/java/MyFiles/index.html");
+                    Path filePath1 = si.getFilePath("src/main/java/MyFiles/index1.json");
+                    Path filePath2 = si.getFilePath("src/main/java/MyFiles/error.html");
+                    if (path.equals("/") && Files.exists(filePath)) {
+                        String contentType = si.guessContentType(filePath);
+                        si.sendResponse(clientSocket, "200 OK", contentType, Files.readAllBytes(filePath));
+                    } else if (path.equals("/json") && Files.exists(filePath1)) {
+                        String contentType = si.guessContentType(filePath1);
+                        si.sendResponse(clientSocket, "200 OK", contentType, Files.readAllBytes(filePath1));
+                    } else {
+                        String contentType = si.guessContentType(filePath2);
+                        si.sendResponse(clientSocket, "200 OK", contentType, Files.readAllBytes(filePath2));
+                    }
+                    System.out.println(requestBuilder.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
         }
+
     }
 }
+
+
--- src/test/java/ServicesTest.java
+++ src/test/java/ServicesTest.java
@@ -1,24 +1,78 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServicesTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void testForRequestMethod() throws IOException {
+        ServerSocket server = new ServerSocket(8082);
+        Socket clientSocket = server.accept();
+        BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while (!(line = br.readLine()).isBlank()) {
+            requestBuilder.append(line).append("\r\n");
+        }
+        String request = requestBuilder.toString();
+        String[] requestsLines = request.split("\r\n");
+        String[] requestLine = requestsLines[0].split(" ");
+        String Actual0utput = requestLine[0];
+        assertEquals("GET", Actual0utput);
+        server.close();
+        clientSocket.close();
     }
 
     @Test
-    void sendResponse() {
-
+    void getFilePath() throws IOException {
+        ServerSocket server = new ServerSocket(8082);
+        Socket clientSocket = server.accept();
+        BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while (!(line = br.readLine()).isBlank()) {
+            requestBuilder.append(line).append("\r\n");
+        }
+        String request = requestBuilder.toString();
+        String[] requestsLines = request.split("\r\n");
+        String[] requestLine = requestsLines[0].split(" ");
+        String path = requestLine[1];
+        assertEquals("/", path);
+        server.close();
+        clientSocket.close();
     }
 
     @Test
-    void getFilePath() {
+    void shouldTestForRequestVersion() throws IOException {
+        ServerSocket server = new ServerSocket(8082);
+        Socket clientSocket = server.accept();
+        BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while (!(line = br.readLine()).isBlank()) {
+            requestBuilder.append(line).append("\r\n");
+        }
+        String request = requestBuilder.toString();
+        String[] requestsLines = request.split("\r\n");
+        String[] requestLine = requestsLines[0].split(" ");
+        String version = requestLine[2];
+        assertEquals("HTTP/1.1", version);
+        server.close();
+        clientSocket.close();
     }
 
     @Test
-    void guessContentType() {
+    void ShouldTestTheContentType() throws IOException {
+        String expected = Files.probeContentType(Path.of("src/main/java/MyFiles/index.html"));
+        assertEquals("text/html", expected);
     }
 }
